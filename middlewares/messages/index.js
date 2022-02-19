837379bd8e0f00c207a603151c00c5daf5723e3b
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-19 08:58:01 -0300

    :pencil: adiciona mensagem de obrigatoriedade de email

--- middlewares/messages/index.js
+++ middlewares/messages/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   DISPLAY_NAME_LENGTH: '"displayName" length must be at least 8 characters long',
   INVALID_EMAIL: '"email" must be a valid email',
+  EMAIL_REQUIRED: '"email" is required',
 };
