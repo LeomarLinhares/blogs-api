c367bd5626ba4c42b30c6146c9d961e6cfd713fc
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-19 09:07:44 -0300

    :construction: separa as responsabilidades de existência e validação de email em funções diferentes

--- middlewares/userValidate.js
+++ middlewares/userValidate.js
@@ -8,10 +8,16 @@ module.exports = {
     next();
   },
 
+  validateIfEmailExists: (req, res, next) => {
+    const { email } = req.body;
+    if (email === undefined) return res.status(400).json({ message: MSG.EMAIL_REQUIRED });
+
+    next();
+  },
+
   validateEmail: (req, res, next) => {
     const { email } = req.body;
     const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    if (email === undefined) return res.status(400).json({ message: MSG.EMAIL_REQUIRED });
     if (!email.match(emailRegex)) return res.status(400).json({ message: MSG.INVALID_EMAIL });
 
     next();
