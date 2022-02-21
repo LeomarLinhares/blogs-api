d0d6c178fb331c7169140072693aef1cd5e16e39
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-21 12:57:16 -0300

    :pencil: adiciona mensagens referentes ao método GET da rota /user/:id

--- middlewares/messages/index.js
+++ middlewares/messages/index.js
@@ -4,8 +4,9 @@ module.exports = {
   EMAIL_REQUIRED: '"email" is required',
   PASSWORD_REQUIRED: '"password" is required',
   INVALID_PASSWORD: '"password" length must be 6 characters long',
-  USER_ALREADY_EXISTS: 'User already registered',
+  USER_ALREADY_EXIST: 'User already registered',
   ER_EMPTY_EMAIL: '"email" is not allowed to be empty',
   ER_EMPTY_PASSWORD: '"password" is not allowed to be empty',
   INVALID_FIELDS: 'Invalid fields',
+  USER_DOES_NOT_EXIST: 'User does not exist',
 };
