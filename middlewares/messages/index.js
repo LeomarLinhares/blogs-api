583593ce26da4d7882a5bd4c2d2b60698816b2cc
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-21 11:05:42 -0300

    :pencil: adiciona mensagens referentes ao método POST da rota /login

--- middlewares/messages/index.js
+++ middlewares/messages/index.js
@@ -5,4 +5,7 @@ module.exports = {
   PASSWORD_REQUIRED: '"password" is required',
   INVALID_PASSWORD: '"password" length must be 6 characters long',
   USER_ALREADY_EXISTS: 'User already registered',
+  ER_EMPTY_EMAIL: '"email" is not allowed to be empty',
+  ER_EMPTY_PASSWORD: '"password" is not allowed to be empty',
+  INVALID_FIELDS: 'Invalid fields',
 };
