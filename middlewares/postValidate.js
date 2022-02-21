39c88b19dcf7f6af9249b528ab609943e1fb396d
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-21 19:44:23 -0300

    :sparkles: cria no módulo postValidate o middleware validateIfInformedCategoryIdExistsInDatabase

--- middlewares/postValidate.js
+++ middlewares/postValidate.js
@@ -1,4 +1,5 @@
 const MSG = require('./messages');
+const { Category } = require('../models');
 
 module.exports = {
   validateIfTitleExists: (req, res, next) => {
@@ -20,6 +21,18 @@ module.exports = {
     if (categoryIds === undefined) {
       return res.status(400).json({ message: MSG.CATEGORY_IDS_REQUIRED });
     }
+  
+    next();
+  },
+
+  validateIfInformedCategoryIdExistsInDatabase: async (req, res, next) => {
+    const { categoryIds } = req.body;
+    const allCategories = await Category.findAll();
+    const allCategoriesExists = categoryIds
+      .reduce((acc, curr) => allCategories
+      .some((category) => category.id === curr) && acc, true);
+    
+    if (!allCategoriesExists) return res.status(400).json({ message: MSG.CATEGORY_ID_NOT_FOUND });
   
     next();
   },
