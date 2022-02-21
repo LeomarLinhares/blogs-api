b8cc18a25718a69ccf4a433d5d918b5c21845820
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-21 19:45:31 -0300

    :construction: adiciona o módulo validatePost no index.js que reúne os middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const loginValidate = require('./loginValidate');
 const userValidate = require('./userValidate');
 const categoriesValidate = require('./categoriesValidate');
+const postValidate = require('./postValidate');
 
 module.exports = {
   ...loginValidate,
   ...userValidate,
   ...categoriesValidate,
+  ...postValidate,
 };
