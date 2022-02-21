5a07a266965a4f51a046523dbad33fc0a45081fa
Leomar Linhares <leomar_linhares@hotmail.com>
2022-02-21 16:12:59 -0300

    :recycle: refatora o módulo categoriesService para capturar os erros em trycatch

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -2,12 +2,20 @@ const categoriesService = require('../services/categoriesService');
 
 module.exports = {
   createCategory: async (req, res) => {
-    const response = await categoriesService.create(req.body);
-    res.status(201).json(response);
+    try {
+      const response = await categoriesService.create(req.body);
+      res.status(201).json(response);
+    } catch (error) {
+      console.log(error);
+    }
   },
 
   getAllCategories: async (_req, res) => {
-    const response = await categoriesService.getAll();
-    res.status(200).json(response);
+    try {
+      const response = await categoriesService.getAll();
+      res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
